6b6adf024d8289f7421affab8ad8e429d492411d
Bowen <bowenduan@users.noreply.github.com>
2020-10-30 20:44:06 +0800

    Update

--- krnl/.SafeboxModule.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/bowen/work/safebox/krnl/SafeboxModule.o := ld -m elf_x86_64  -z max-page-size=0x200000   -r -o /home/bowen/work/safebox/krnl/SafeboxModule.o /home/bowen/work/safebox/krnl/sdthook.o /home/bowen/work/safebox/krnl/syscalltable.o /home/bowen/work/safebox/krnl/netlinkp.o /home/bowen/work/safebox/krnl/overload.o 
--- krnl/.tmp_versions/SafeboxModule.mod
+++
@@ -1,3 +0,0 @@
-/home/bowen/work/safebox/krnl/SafeboxModule.ko
-/home/bowen/work/safebox/krnl/sdthook.o /home/bowen/work/safebox/krnl/syscalltable.o /home/bowen/work/safebox/krnl/netlinkp.o /home/bowen/work/safebox/krnl/overload.o
-
