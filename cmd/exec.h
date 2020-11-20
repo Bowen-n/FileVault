9ef4f3b88b04d5c0b3d003e412f4fb906c840943
Niemj8919 <43309181+Niemj8919@users.noreply.github.com>
2020-11-20 22:24:01 +0800

    Update exec.h
    Add commands

--- cmd/exec.h
+++ cmd/exec.h
@@ -13,6 +13,10 @@
 #define CMD_CD "cd"
 #define CMD_LS "ls"
 #define CMD_EXIT "exit"
+#define CMD_RM "rm"
+#define CMD_MKDIR "mkdir"
+#define CMD_MVIN "mvin"
+#define CMD_MVOUT "mvout"
 
 char pwd[MAX_PATH_LEN];
 char root[MAX_PATH_LEN];
@@ -20,10 +24,17 @@ char display_pwd[MAX_PATH_LEN];
 
 // help functions
 int _ls(char path[COMMAND_MAX_LEN]);
+int _cd(char path[COMMAND_MAX_LEN]);
+int _mkdir(char path[COMMAND_MAX_LEN]);
+int _rm(char path[COMMAND_MAX_LEN]);
 
 // special execute
 int exec_cd(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
 int exec_ls(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
+int exec_rm(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
+int exec_mkdir(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
+int exec_mvin(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
+int exec_mvout(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
 
 // main execute
 int execute(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
