6686ac305b461176f4f59c4d05ad56072d849eee
Niemj8919 <43309181+Niemj8919@users.noreply.github.com>
2020-11-22 17:51:13 +0800

    Update exec.h

--- cmd/exec.h
+++ cmd/exec.h
@@ -15,8 +15,10 @@
 #define CMD_EXIT "exit"
 #define CMD_RM "rm"
 #define CMD_MKDIR "mkdir"
+#define CMD_MV "mv"
 #define CMD_MVIN "mvin"
 #define CMD_MVOUT "mvout"
+
 
 char pwd[MAX_PATH_LEN];
 char root[MAX_PATH_LEN];
@@ -27,12 +29,15 @@ int _ls(char path[COMMAND_MAX_LEN]);
 int _cd(char path[COMMAND_MAX_LEN]);
 int _mkdir(char path[COMMAND_MAX_LEN]);
 int _rm(char path[COMMAND_MAX_LEN]);
+int _mv(char oldfile[COMMAND_MAX_LEN], char newfile[COMMAND_MAX_LEN]);
+
 
 // special execute
 int exec_cd(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
 int exec_ls(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
 int exec_rm(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
 int exec_mkdir(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
+int exec_mv(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
 int exec_mvin(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
 int exec_mvout(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count);
 
