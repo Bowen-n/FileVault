d186c514179e4695abb7ccd155a89309971c81f3
Niemj8919 <43309181+Niemj8919@users.noreply.github.com>
2020-11-26 14:31:22 +0800

    Update exec.c

--- cmd/exec.c
+++ cmd/exec.c
@@ -1,7 +1,7 @@
 #include "exec.h"
+#include "base64.h"
 
-
-int _cd(char path[COMMAND_MAX_LEN])
+int _cd(char path[MAX_PATH_LEN])
 {
     return chdir(path);
 }
@@ -20,9 +20,9 @@ int exec_cd(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
         }
         case 2:
         {
-            char cd_path[COMMAND_MAX_LEN]; memset(cd_path, 0, COMMAND_MAX_LEN);
-            char display_path[COMMAND_MAX_LEN]; memset(display_path, 0, COMMAND_MAX_LEN);
-            char target_path[COMMAND_MAX_LEN]; memset(target_path, 0, COMMAND_MAX_LEN);
+            char cd_path[MAX_PATH_LEN]; memset(cd_path, 0, MAX_PATH_LEN);
+            char display_path[MAX_PATH_LEN]; memset(display_path, 0, MAX_PATH_LEN);
+            char target_path[MAX_PATH_LEN]; memset(target_path, 0, MAX_PATH_LEN);
             strcpy(cd_path, splited_cmd[1]);
             
             if (strncmp(cd_path, "/", 1) == 0)//abspath
@@ -30,7 +30,7 @@ int exec_cd(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
                 strcpy(target_path, root);
                 strcat(target_path, cd_path);
                 //strcpy(display_path, display_pwd);
-		memset(display_path, 0, COMMAND_MAX_LEN);                
+		memset(display_path, 0, MAX_PATH_LEN);                
 		strcpy(display_path, cd_path);
                 
                 int suc = _cd(target_path);
@@ -40,9 +40,9 @@ int exec_cd(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
                 }
                 else
                 {
-                    memset(display_pwd, 0, COMMAND_MAX_LEN); // change display path
+                    memset(display_pwd, 0, MAX_PATH_LEN); // change display path
                     strcpy(display_pwd, display_path);
-		    memset(pwd, 0, COMMAND_MAX_LEN);
+		    memset(pwd, 0, MAX_PATH_LEN);
 		    strcpy(pwd, root);
 		    strcat(pwd, display_pwd);   
                 }
@@ -53,10 +53,9 @@ int exec_cd(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
                 strcpy(target_path, pwd);
                 strcat(target_path, "/");
                 strcat(target_path, cd_path); 
-                char norm[COMMAND_MAX_LEN]; memset(norm, 0, COMMAND_MAX_LEN);
+                char norm[MAX_PATH_LEN]; memset(norm, 0, MAX_PATH_LEN);
                 
                 normpath(norm, target_path);
-		//printf("%s\n", norm);
                 
                 if (strncmp(norm, root, strlen(root)) != 0) //root doesn't have parent directory.
                 {
@@ -67,7 +66,7 @@ int exec_cd(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
                 strcpy(display_path, display_pwd);
 		strcat(display_path, "/");
                 strcat(display_path, cd_path);
-                char norm_display[COMMAND_MAX_LEN]; memset(norm_display, 0, COMMAND_MAX_LEN);
+                char norm_display[MAX_PATH_LEN]; memset(norm_display, 0, MAX_PATH_LEN);
                 
                 normpath(norm_display, display_path);
                 
@@ -78,13 +77,12 @@ int exec_cd(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
                 }
                 else
                 {
-                    memset(display_pwd, 0, COMMAND_MAX_LEN); // change display path
+                    memset(display_pwd, 0, MAX_PATH_LEN); // change display path
                     strcpy(display_pwd, norm_display);
-		    memset(pwd, 0, COMMAND_MAX_LEN);
+		    memset(pwd, 0, MAX_PATH_LEN);
 		    strcpy(pwd, root);
 		    strcat(pwd, norm_display);
                 }
-		//printf("%s\n", pwd);
                 break;
             }
         default:
@@ -94,7 +92,7 @@ int exec_cd(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
     return 0;
 }
 
-int _rm(char file[COMMAND_MAX_LEN])
+int _rm(char file[MAX_PATH_LEN])
 {
     return remove(file);
 }
@@ -110,9 +108,9 @@ int exec_rm(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
         }
         case 2:
         {
-            char rm_file[COMMAND_MAX_LEN]; memset(rm_file, 0, COMMAND_MAX_LEN);
-            char target_file[COMMAND_MAX_LEN]; memset(target_file, 0, COMMAND_MAX_LEN);
-	    char display_path[COMMAND_MAX_LEN]; memset(display_path, 0, COMMAND_MAX_LEN);
+            char rm_file[MAX_PATH_LEN]; memset(rm_file, 0, MAX_PATH_LEN);
+            char target_file[MAX_PATH_LEN]; memset(target_file, 0, MAX_PATH_LEN);
+	    char display_path[MAX_PATH_LEN]; memset(display_path, 0, MAX_PATH_LEN);
             strcpy(rm_file, splited_cmd[1]);
             
             if (strncmp(rm_file, "/", 1) == 0)
@@ -131,17 +129,16 @@ int exec_rm(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
                 strcpy(target_file, display_pwd);
                 strcat(target_file, "/");
                 strcat(target_file, rm_file); 
-                char norm[COMMAND_MAX_LEN]; memset(norm, 0, COMMAND_MAX_LEN);
+                char norm[MAX_PATH_LEN]; memset(norm, 0, MAX_PATH_LEN);
                     
                 normpath(norm, target_file);
 		
-		char full_path[COMMAND_MAX_LEN]; memset(full_path, 0, COMMAND_MAX_LEN);
+		char full_path[MAX_PATH_LEN]; memset(full_path, 0, MAX_PATH_LEN);
 		strcpy(full_path, root);
 		strcat(full_path, norm);
-		char full_norm[COMMAND_MAX_LEN]; memset(full_norm, 0, COMMAND_MAX_LEN); 
+		char full_norm[MAX_PATH_LEN]; memset(full_norm, 0, MAX_PATH_LEN); 
 		normpath(full_norm, full_path);		
 		
-		//printf("%s\n", full_norm);
                     
                 if (strncmp(full_norm, root, strlen(root)) != 0) //can't remove file which isn't under user's pwd.
                 {
@@ -151,7 +148,7 @@ int exec_rm(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
                     
                 strcpy(display_path, display_pwd);
                 strcat(display_path, rm_file);
-                char norm_display[COMMAND_MAX_LEN]; memset(norm_display, 0, COMMAND_MAX_LEN);
+                char norm_display[MAX_PATH_LEN]; memset(norm_display, 0, MAX_PATH_LEN);
                     
                 normpath(norm_display, display_path);
                     
@@ -171,7 +168,7 @@ int exec_rm(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
     return 0;
 }
 
-int _mkdir(char path[COMMAND_MAX_LEN])
+int _mkdir(char path[MAX_PATH_LEN])
 {
     return mkdir(path,0777);
 }
@@ -187,9 +184,9 @@ int exec_mkdir(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
             }
         case 2:
             {
-                char mk_dir[COMMAND_MAX_LEN]; memset(mk_dir, 0, COMMAND_MAX_LEN);
+                char mk_dir[MAX_PATH_LEN]; memset(mk_dir, 0, MAX_PATH_LEN);
                 strcpy(mk_dir, splited_cmd[1]);
-		char target_dir[COMMAND_MAX_LEN]; memset(target_dir, 0, COMMAND_MAX_LEN);                
+		char target_dir[MAX_PATH_LEN]; memset(target_dir, 0, MAX_PATH_LEN);                
 		//strcpy(target_dir, display_pwd);
 		//strcat(target_dir, "/");		
 		//strcat(target_dir, mk_dir);
@@ -216,8 +213,6 @@ int exec_mkdir(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
 
 int _mv(char oldfile[MAX_PATH_LEN], char newfile[MAX_PATH_LEN])
 {
-    //printf("%s\n", oldfile);
-    //printf("%s\n", newfile);
     //if (link(oldfile, newfile) == 0)
     //{
 	//if (unlink(oldfile) == 0)
@@ -238,7 +233,7 @@ int _mv(char oldfile[MAX_PATH_LEN], char newfile[MAX_PATH_LEN])
     return rename(oldfile, newfile);
 }
 
-int in_or_out(char path[COMMAND_MAX_LEN]) //inside or outside the vault.
+int in_or_out(char path[MAX_PATH_LEN]) //inside or outside the vault.
 {
     if (strncmp(path, root, strlen(root)) != 0) //outside
     {
@@ -321,8 +316,8 @@ int exec_mvin(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
     {
 	case 3:
 	{
-	    char old_file[COMMAND_MAX_LEN]; memset(old_file, 0, COMMAND_MAX_LEN);
-            char new_file[COMMAND_MAX_LEN]; memset(new_file, 0, COMMAND_MAX_LEN);
+	    char old_file[MAX_PATH_LEN]; memset(old_file, 0, MAX_PATH_LEN);
+            char new_file[MAX_PATH_LEN]; memset(new_file, 0, MAX_PATH_LEN);
 	    char target_newfile[MAX_PATH_LEN]; memset(target_newfile, 0, MAX_PATH_LEN);
             strcpy(old_file, splited_cmd[1]);
             strcpy(new_file, splited_cmd[2]);
@@ -341,7 +336,7 @@ int exec_mvin(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
 		}
 		else
 		{
-		    char temp_newfile[COMMAND_MAX_LEN]; memset(temp_newfile, 0, COMMAND_MAX_LEN);
+		    char temp_newfile[MAX_PATH_LEN]; memset(temp_newfile, 0, MAX_PATH_LEN);
                     strcpy(temp_newfile, pwd);
                     strcat(temp_newfile, "/");
                     strcat(temp_newfile, new_file); 
@@ -378,8 +373,8 @@ int exec_mvout(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
     {
 	case 3:
 	{
-	    char old_file[COMMAND_MAX_LEN]; memset(old_file, 0, COMMAND_MAX_LEN);
-            char new_file[COMMAND_MAX_LEN]; memset(new_file, 0, COMMAND_MAX_LEN);
+	    char old_file[MAX_PATH_LEN]; memset(old_file, 0, MAX_PATH_LEN);
+            char new_file[MAX_PATH_LEN]; memset(new_file, 0, MAX_PATH_LEN);
 	    char target_oldfile[MAX_PATH_LEN]; memset(target_oldfile, 0, MAX_PATH_LEN);
             strcpy(old_file, splited_cmd[1]);
             strcpy(new_file, splited_cmd[2]);
@@ -398,7 +393,7 @@ int exec_mvout(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
 		}
 		else
 		{
-		    char temp_oldfile[COMMAND_MAX_LEN]; memset(temp_oldfile, 0, COMMAND_MAX_LEN);
+		    char temp_oldfile[MAX_PATH_LEN]; memset(temp_oldfile, 0, MAX_PATH_LEN);
                     strcpy(temp_oldfile, pwd);
                     strcat(temp_oldfile, "/");
                     strcat(temp_oldfile, old_file); 
@@ -432,7 +427,7 @@ int exec_mvout(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
 
 
 
-int _ls(char path[COMMAND_MAX_LEN])
+int _ls(char path[MAX_PATH_LEN])
 {
     DIR* dp; struct dirent* ep;
     dp = opendir(path);
@@ -470,9 +465,9 @@ int exec_ls(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
         case 2:
         {
             DIR* dp; struct dirent* ep;
-            char ls_path[COMMAND_MAX_LEN]; memset(ls_path, 0, COMMAND_MAX_LEN);
-            char display_path[COMMAND_MAX_LEN]; memset(display_path, 0, COMMAND_MAX_LEN);
-            char target_path[COMMAND_MAX_LEN]; memset(target_path, 0, COMMAND_MAX_LEN);
+            char ls_path[MAX_PATH_LEN]; memset(ls_path, 0, MAX_PATH_LEN);
+            char display_path[MAX_PATH_LEN]; memset(display_path, 0, MAX_PATH_LEN);
+            char target_path[MAX_PATH_LEN]; memset(target_path, 0, MAX_PATH_LEN);
             strcpy(ls_path, splited_cmd[1]);
 
             // ls_path is abspath
@@ -492,7 +487,7 @@ int exec_ls(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
                 strcpy(target_path, pwd);
                 strcat(target_path, "/");
                 strcat(target_path, ls_path); // /home/safebox/bowen/./ddd
-                char norm[COMMAND_MAX_LEN]; memset(norm, 0, COMMAND_MAX_LEN);
+                char norm[MAX_PATH_LEN]; memset(norm, 0, MAX_PATH_LEN);
                 normpath(norm, target_path);
                 if (strncmp(norm, root, strlen(root)) != 0)
                 {
@@ -502,7 +497,7 @@ int exec_ls(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
                 
                 strcpy(display_path, display_pwd);
                 strcat(display_path, ls_path);
-                char norm_display[COMMAND_MAX_LEN]; memset(norm_display, 0, COMMAND_MAX_LEN);
+                char norm_display[MAX_PATH_LEN]; memset(norm_display, 0, MAX_PATH_LEN);
                 normpath(norm_display, display_path);
                 int suc = _ls(norm);
                 if (suc == -1)
@@ -517,6 +512,118 @@ int exec_ls(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
 
     } // switch
 
+    return 0;
+}
+
+int check_pswd(char old_pswd[MAX_PATH_LEN])
+{
+	FILE *fp;
+	char *buf_pswd = (char*)malloc(100);
+	char *buf_ID = (char*)malloc(100);
+	base64_encode(old_pswd, &buf_pswd);
+	char ID[100]; memset(ID, 0, 100);
+        char password[100]; memset(password, 0, 100);
+
+	fp = fopen("/home/safebox/password.dat", "r+");
+        
+        while(fscanf(fp, "%s %s", ID, password))
+        {
+	    base64_decode(ID, &buf_ID);
+	    if (atoi(buf_ID) == getuid())
+	    {
+	        if(!(strcmp(password, buf_pswd)))
+	        {
+		    fclose(fp);
+		    return 0;
+	        }
+	        else
+	        {
+		    printf("wrong old password.");
+		    fclose(fp);
+		    return -1;
+	        }
+	    }
+
+	    if (feof(fp))
+	    {
+		fclose(fp);
+		return -1;
+    	    }
+        }
+	fclose(fp);
+	return -1;
+}
+
+int exec_chpswd(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
+{
+    FILE *fp;
+    FILE *new_fp;
+    char *buf_ID;
+    char ID[100]; memset(ID, 0, 100);
+    char password[100]; memset(password, 0, 100);
+    
+
+    switch(cmd_count)
+    {
+	case 1:
+	{
+	    
+	    if (!(fp = fopen("/home/safebox/password.dat", "r+")))
+	    {
+		printf("Error openning password.dat.\n");
+		break;
+	    }
+
+	    if (!(new_fp = fopen("/home/safebox/password_temp.dat", "w")))
+	    {
+		printf("Error openning password_temp.dat.\n");
+		break;
+	    }
+
+	    else
+	    {
+		
+		while(fscanf(fp, "%s", ID))
+		{
+		    base64_decode(ID, &buf_ID);
+		    if(atoi(buf_ID) == getuid())
+		    {
+			printf("enter new password.\n");
+			char *new_pswd = (char*)malloc(100);
+			char *buf_new = (char*)malloc(100);
+			scanf("%s", new_pswd); getchar();
+			
+			fprintf(new_fp, "%s", ID);
+			free(buf_ID);
+			base64_encode(new_pswd, &buf_new);
+			fprintf(new_fp, " %s\n", buf_new);
+			free(buf_new);
+			break;
+		    }
+		    else
+		    {
+			fprintf(new_fp, "%s", ID);
+			fscanf(fp, " %s", password);
+			fprintf(new_fp, " %s\n", password);
+		    }
+		    if(feof(fp))
+		    {
+			break;
+		    }
+		}
+		remove("/home/safebox/password.dat");
+		rename("/home/safebox/password_temp.dat", "/home/safebox/password.dat");
+	    }
+	    fclose(fp);
+	    fclose(new_fp);
+	    printf("success.\n");
+	    break;
+	}
+	default:
+	{
+	    printf("argument error.\n");
+	}
+    }
     return 0;
 }
 
@@ -540,6 +647,8 @@ int execute(char splited_cmd[][COMMAND_MAX_LEN], int cmd_count)
 	return exec_mvin(splited_cmd, cmd_count);
     else if(strcmp(main_cmd, CMD_MVOUT) == 0)
 	return exec_mvout(splited_cmd, cmd_count);
+    else if(strcmp(main_cmd, CMD_CHPSWD) == 0)
+	return exec_chpswd(splited_cmd, cmd_count);
     else if(strcmp(main_cmd, CMD_EXIT) == 0)
         return -1;
     else
